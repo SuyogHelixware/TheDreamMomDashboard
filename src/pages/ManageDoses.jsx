bfb4c5713ccdae05f71ca5bffb322de8821f568f
JuberNInamdar <133654194+JuberNInamdar@users.noreply.github.com>
2024-05-25 11:36:34 +0530

    Update ManageDoses.jsx

--- src/pages/ManageDoses.jsx
+++ src/pages/ManageDoses.jsx
@@ -75,7 +75,7 @@ const ManageDoses = () => {
 
   const handleSubmitForm = () => {
     const requiredFields = ["Name", "Description"];
-    const emptyRequiredFields = requiredFields.filter((field) => !data[field].trim());
+    const emptyRequiredFields = requiredFields.filter( (field) => !data[field] || !data[field].trim());
     if (emptyRequiredFields.length > 0) {
       validationAlert("Please fill in all required fields");
       return;
